51a694e2036dc812788c7d76e219390a3c7fd328
a1d3nmam3d0v <89761421+a1d3nmam3d0v@users.noreply.github.com>
2021-12-05 16:12:43 -0600

    commmit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1>Would you rather?</h1>
+
+  <would-you-rather 
+    v-for="(question) in questions"
+    v-bind:key="question.id" 
+    v-bind:question="question.question">
+  </would-you-rather>
+
+</div>
+
 </template>
 
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WouldYouRather
+    },
+    data() {
+      return {
+        questions: [
+          {id: 0,
+          question1: 'Would you rather be a robot or a cyborg?',
+          answer1: 'Robot',
+          answer2: 'Cyborg',
+          },
+          {id: 1,
+          question: 'Would you rather be rich and unknown or famous but not rich?',
+          answer1: 'Rich and unknown',
+          answer2: 'Famous but not rich',
+          },
+          {id: 2,
+          question: 'Would you rather swim in Jell-O or swim in Nutella?',
+          answer1: 'Jell-o',
+          answer2: 'Nutella'
+          }
+        ]
+      }
+    }
 }
+
 </script>
 
+
+
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="wyr">
+
+<h2>Choose 1</h2>
+<h3>{{ question }}</h3>
+
+<input
+    type="radio" v-model="choice" 
+    v-bind:value="answer1" v-on:change="choiceMade">
+<label>{{ answer1 }}</label>
+
+<input
+    type="radio" v-model="choice"
+    v-bind:value="answer2" v-on:change="choiceMade">
+    
+    <label>{{ answer2 }}</label>
+
+
+</div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'WouldYouRather',
+
+    props: {
+        question: String,
+        answer1: String,
+        answer2: String
+    },
+    data() {
+        return {
+            choice: ''
+        }
+    },
+    methods: {
+    choiceMade() {
+        this.$emit('answer-changed',this.choice)
+        }
+    }
+}
+
+</script>
