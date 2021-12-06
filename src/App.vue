37dbff44044e11792d80f145842d18438d075f46
a1d3nmam3d0v <89761421+a1d3nmam3d0v@users.noreply.github.com>
2021-12-05 21:19:58 -0600

    applied styles

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,103 @@
 <template>
-  <div id="app">
+	<div id="app">
+		<h1>Would you rather?</h1>
 
-    <h1>Would you rather?</h1>
+		<would-you-rather
+			v-for="question in questions"
+			v-bind:key="question.id"
+			v-bind:question="question.question"
+			v-bind:answer1="question.answer1"
+			v-bind:answer2="question.answer2"
+			v-on:user-chose="userChose"
+		>
+		</would-you-rather>
 
-  <would-you-rather 
-    v-for="(question) in questions"
-    v-bind:key="question.id" 
-    v-bind:question="question.question"
-    v-bind:answer1="question.answer1"
-    v-bind:answer2="question.answer2"
-    v-on:user-chose="userChose">
-  </would-you-rather>
-  
-<p id=YouWouldRather>You would rather</p>
+		<p class="you-would-rather">You would rather</p>
 
-<ul id="usersChoices">
-<li v-for="(choice) in usersChoices">
-  {{ choice }}
-</li>
-</ul>
-
-
-
-
-
-</div>
-
+		<ul id="usersChoices">
+			<li v-for="choice in usersChoices">
+				{{ choice }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
-  name: 'App',
-  components: {
-    WouldYouRather
-    },
-    data() {
-      return {
-        questions: [
-          {id: 0,
-          question: 'Would you rather be a robot or a cyborg?',
-          answer1: 'Robot',
-          answer2: 'Cyborg',
-          },
-          {id: 1,
-          question: 'Would you rather be rich and unknown or famous but not rich?',
-          answer1: 'Rich and unknown',
-          answer2: 'Famous but not rich',
-          },
-          {id: 2,
-          question: 'Would you rather swim in Jell-O or swim in Nutella?',
-          answer1: 'Jell-o',
-          answer2: 'Nutella'
-          }],
+	name: 'App',
+	components: {
+		WouldYouRather,
+	},
+	data() {
+		return {
+			questions: [
+				{
+					id: 0,
+					question: 'Would you rather be a robot or a cyborg?',
+					answer1: 'robot',
+					answer2: 'Cyborg',
+				},
+				{
+					id: 1,
+					question:
+						'Would you rather be rich and unknown or famous but not rich?',
+					answer1: 'Rich and unknown',
+					answer2: 'Famous but not rich',
+				},
+				{
+					id: 2,
+					question: 'Would you rather swim in Jell-O or swim in Nutella?',
+					answer1: 'Jell-o',
+					answer2: 'Nutella',
+				},
+			],
 
-          usersChoices: []
-      }
-    },
-    methods: {
-      userChose(choice) {
-        // if (this.usersChoices.includes(choice)) {
-        //   return
-        //   } else {
-            this.usersChoices.push(choice)
-      }}
-    }
-
+			usersChoices: [],
+		}
+	},
+	methods: {
+		userChose(choice) {
+			this.usersChoices.push(choice)
+		},
+	},
+}
 </script>
 
-
-
-
-
-
 <style>
+.you-would-rather {
+	font-family: impact;
+	font-style: italic;
+	margin-bottom: 20px;
+}
 
+body {
+	background: burlywood;
+}
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	margin-top: 60px;
+}
+h1 {
+	font-style: oblique;
+	text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+		1px 1px 0 #000;
+}
+p {
+	font-size: 45px;
+	font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+	color: black;
+}
+p:hover {
+	color: lime;
+	font-style: italic;
+	text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+		1px 1px 0 #000;
+	text-decoration: underline;
+	text-decoration-color: orangered;
+}
 </style>
--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -1,45 +1,81 @@
 <template>
-<div class="wyr">
+	<div class="wyr">
+		<h3>{{ question }}</h3>
 
-<h2>Choose one</h2>
-<h3>{{ question }}</h3>
+		<input
+			type="radio"
+			v-model="choice"
+			v-bind:value="answer1"
+			v-on:change="choiceMade"/>
+		<label>{{ answer1 }}</label>
 
-<input
-    type="radio" v-model="choice" 
-    v-bind:value="answer1" v-on:change="choiceMade">
-<label>{{ answer1 }}</label>
+		<input
+			type="radio"
+			v-model="choice"
+			v-bind:value="answer2"
+			v-on:change="choiceMade"/>
 
-<input  
-    type="radio" v-model="choice"
-    v-bind:value="answer2" v-on:change="choiceMade">
-    
-    <label>{{ answer2 }}</label>
-
-
-</div>
+		<label>{{ answer2 }}</label>
+	</div>
 </template>
 
-
 <script>
-
 export default {
-    name: 'WouldYouRather',
+	name: 'WouldYouRather',
 
-    props: {
-        question: String,
-        answer1: String,
-        answer2: String
-    },
-    data() {
-        return {
-            choice: ''
-        }
-    },
-    methods: {
-    choiceMade() {
-        this.$emit('user-chose',this.choice)
-        }
-    }
+	props: {
+		question: String,
+		answer1: String,
+		answer2: String,
+	},
+	data() {
+		return {
+			choice: '',
+		}
+	},
+	methods: {
+		choiceMade() {
+			this.$emit('user-chose', this.choice)
+		},
+	},
+}
+</script>
+
+<style>
+h2 {
+	font-size: 13px;
 }
 
-</script>
+h3 {
+	margin: 40px 0 0;
+}
+.wyr {
+	border: 2px black solid;
+	background: orangered;
+}
+.wyr:hover h3 {
+	font-size: 30px;
+	font-family: arial;
+	color: white;
+	text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+		1px 1px 0 #000;
+	transform: scale(1) rotate(0deg) translate(0px, 0px) skew(20deg, 0deg);
+}
+h2 {
+	color: white;
+	font-size: 14px;
+	font-variant: small-caps;
+}
+.wyr:hover h2 {
+	text-decoration: underline;
+	color: black;
+}
+h3 {
+	line-height: 30px;
+	margin-top: 20px;
+	margin-bottom: 10px;
+	color: white;
+	transform: scale(1) rotate(0deg) translate(0px, 0px) skew(30deg, 0deg);
+	font-size: 30px;
+}
+</style>
